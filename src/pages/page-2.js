2a7cecefd1149e4e21b69e818b153154dbef673e
Marcus Little <marcuslittle@marcuss-mbp-2.lan>
2023-06-08 13:06:28 -0400

    add search box

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,36 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { navigate } from 'gatsby';
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+export const SearchBox = () => {
+  const [search, setSearch] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/search?query=${encodeURIComponent(search)}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
 
 const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <SearchBox />
   </Layout>
 )
 
